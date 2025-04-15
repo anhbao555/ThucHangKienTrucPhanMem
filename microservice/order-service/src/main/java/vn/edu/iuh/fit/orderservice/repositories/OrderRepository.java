fd42a2c5655d7ee01e3c2a36c4b9179e6ea33d25
TAB <121606280+anhbao555@users.noreply.github.com>
2025-04-15 16:41:15 +0700

    Delete microservice directory (clone)

--- microservice/order-service/src/main/java/vn/edu/iuh/fit/orderservice/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.iuh.fit.orderservice.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import vn.edu.iuh.fit.orderservice.entities.Order;
-
-@Repository
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
--- microservice/product-service/src/main/java/vn/edu/iuh/fit/productservice/repositories/ProductRepostory.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.iuh.fit.productservice.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import vn.edu.iuh.fit.productservice.entities.Product;
-
-@Repository
-public interface ProductRepostory extends MongoRepository<Product, Long> {
-}
